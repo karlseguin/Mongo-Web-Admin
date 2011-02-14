7286ce7b44c539b3f6abade745086eaa6876a465
Karl Seguin <karlseguin@gmail.com>
2011-02-14 21:32:58 +0800

    block access to non local requests for now

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :ensure_local
+  
+  def ensure_local
+    render :text => 'request must be local', :status => 400 unless request.local?
+  end
+  
 end
