630af455637ecb5f03982796111b2223425a0129
Karl Seguin <karlseguin@gmail.com>
2011-02-17 23:21:18 +0800

    can be configured via vars

--- lib/settings.rb
+++ lib/settings.rb
@@ -2,12 +2,12 @@ class Settings
   @@settings = YAML::load_file(Rails.root + 'config/config.yml') || Hash.new
   
   def self.local_only?
-    @@settings.has_key?('local_only') ? @@settings['local_only'] : true
+    @@settings.has_key?('local_only') ? @@settings['local_only'] : ENV['LOCAL_ONLY'] || true
   end  
   def self.auto_connect
-    @@settings['auto_connect']
+    @@settings['auto_connect'] || ENV['MONGO_URL']
   end
   def self.databases
-    @@settings['databases']
+    @@settings['databases'] || ENV['MONGO_DATABASES']
   end
 end
